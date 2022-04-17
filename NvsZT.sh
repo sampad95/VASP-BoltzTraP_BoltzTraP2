b6989451945e8465b56d7152842ff4c9fffbb489
SamapdMandal <sampadmandals@gmail.com>
2022-04-17 07:31:15 +0530

    Add files via upload

---
+++ NvsZT.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "Enter the temperature"
+read temp
+echo "Enter the trace file"
+read trace_file
+grep $temp $trace_file > "${temp%%.*}".dat
+#echo "Enter the name of the file"
+#read file                # 'file' is T.dat, where T is the temperature
+#awk '{print $1 = $3, $2 = ($5*$5*$6)}' $file >  "${file%%.*}"_NvsPF.dat 
+awk '{print $1 = $3, $2 = (($5*$5*$6*$2)/$8)}' "${temp%%.*}".dat >  "${temp%%.*}"_NvsZT.dat
